2e28a6bab3b52c9150dd5c30e9f119031d847d37
Dacst12 <misiya3@yandex.ru>
2025-01-17 23:15:06 +0500

    small fix in authorization

--- js/authorization.js
+++ js/authorization.js
@@ -6,19 +6,19 @@ const forms = document.querySelectorAll('.authorization-container form');
 
 const tl = gsap.timeline({ paused: true });
 
-tl.to(welcomeWrapper, {duration: 1.4, ease: "power2.inOut", left: "50%"})
+tl.to(welcomeWrapper, {duration: 1.4, ease: "power2.inOut", left: "0%"})
 .to(welcomeWrapper, {duration: 0.7, ease: "power2.inOut", width: "57%", onComplete: switchContent}, "-=1.4")
 .to(welcomeWrapper, {duration: 0.7, ease: "power2.inOut", width: "50%", onReverseComplete: switchContent}, "-=0.73")
-.to(authorizationWrapper, {duration: 1.4, ease: "power2.inOut", left: "0%"}, "-=1.4")
+.to(authorizationWrapper, {duration: 1.4, ease: "power2.inOut", left: "50%"}, "-=1.4")
 
 const textStage = {
-    'enter': 'Пожалуйста, войдите в систему, указав свои персональные данные',
-    'register': 'Введите свои персональные данные и начните тренироваться с нами',
+    'register': 'Пожалуйста, войдите в систему, указав свои персональные данные',
+    'enter': 'Введите свои персональные данные и начните тренироваться с нами',
 };
 
 const buttonStage = {
-    'enter': 'Войти',
-    'register': 'Зарегистрироваться',
+    'register': 'Войти',
+    'enter': 'Зарегистрироваться',
 };
 
 const changeContent = (element, dictionary) => {
